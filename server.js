68f8655c5a568f8bd9c6d5fb2a6da26f6ffd71b1
Abhiraj Intern <confideleapcrm@gmail.com>
2026-07-06 12:33:54 +0530

    2nd commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
         origin: [
             "http://localhost:5173",
-            "https://thunderous-stroopwafel-86ccbc.netlify.app/api"
+            "https://thunderous-stroopwafel-86ccbc.netlify.ap"
         ],
         credentials: true,
     }));
