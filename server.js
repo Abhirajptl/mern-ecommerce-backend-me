58537a19acc602d7b253d3f916e5ec2410f01381
Abhiraj Intern <confideleapcrm@gmail.com>
2026-07-06 12:13:13 +0530

    frontend allowed access cors

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
         origin: [
             "http://localhost:5173",
+            "https://mern-ecommerce-backend-me.onrender.com/api"
         ],
         credentials: true,
     }));
