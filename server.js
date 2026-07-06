61006409389521e7a4916572e5ba7d61876e91d7
Abhiraj Intern <confideleapcrm@gmail.com>
2026-07-06 12:17:58 +0530

    latest commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
         origin: [
             "http://localhost:5173",
-            "https://mern-ecommerce-backend-me.onrender.com/api"
+            "https://thunderous-stroopwafel-86ccbc.netlify.app/api"
         ],
         credentials: true,
     }));
